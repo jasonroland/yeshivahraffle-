fb599790d08404b80a67941d297329e9fb4ce2fb
Jason Roland <1jasonroland@gmail.com>
2025-11-09 18:25:24 -0500

    remove lint

--- src/types/authorizenet.d.ts
+++ src/types/authorizenet.d.ts
@@ -1,5 +1,7 @@
 declare module 'authorizenet' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export const APIContracts: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export const APIControllers: any;
   export default APIContracts;
 }
